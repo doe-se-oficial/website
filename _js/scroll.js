90938f8da0e7e99386fa579130b7062fc6efe7f0
Lucas Otaviani Bergamo de Oliveira <47718917+LucasOtaviani@users.noreply.github.com>
2020-01-25 21:22:00 -0300

    Revert "The first version of the website! (#1)"
    This reverts commit 5f525f771fb3e7cd2d03a5d4a22751b8400b96b5.

--- _js/scroll.js
+++
@@ -1,7 +0,0 @@
-/*Função para rolar suave*/
-jQuery(document).ready(function($) { 
-    $(".scroll").click(function(event){        
-        event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-    });
-});
