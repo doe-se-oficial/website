5f525f771fb3e7cd2d03a5d4a22751b8400b96b5
Lucas Otaviani Bergamo de Oliveira <47718917+LucasOtaviani@users.noreply.github.com>
2020-01-25 21:19:57 -0300

    The first version of the website! (#1)

---
+++ _js/import.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.src = 'https://code.jquery.com/jquery-3.3.1.slim.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
