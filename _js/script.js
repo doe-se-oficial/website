fe220aaee9696756359b75726484d8299283e2be
Lucas Otaviani <lucas.otaviani@hotmail.com>
2020-06-28 21:10:03 -0300

    Updated the principal image of the website, optimized the css and scripts, now we need to make the news page fun

--- _js/script.js
+++ _js/script.js
@@ -1,7 +1,14 @@
-/*Função para rolar suave*/
 jQuery(document).ready(function($) { 
     $(".scroll").click(function(event){        
         event.preventDefault();
         $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+    });
+});
+
+$(function() { 
+    var navMain = $(".navbar-collapse");
+
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
     });
 });
